79e5d0776d6f91cea8e9d36709440da2ddd92560
Christopher-Kelly <chrissyk99@hotmail.co.uk>
2023-06-08 21:33:30 +0100

    4/4, needs refactoring

--- Car Sharing/task/src/carsharing/Database.java
+++ Car Sharing/task/src/carsharing/Database.java
@@ -1,16 +1,14 @@
 package carsharing;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Database {
     private static Database db = null;
     static final String JDBC_DRIVER = "org.h2.Driver";
     private String DB_URL = "jdbc:h2:file:../task/src/carsharing/db/";
-    private final Connection connection;
+    private static Connection connection = null;
 
     public static Database createDB(String dbName) {
         if (db == null) {
@@ -40,20 +38,21 @@ public class Database {
     public void closeDB() {
         try {
             if (connection != null) {
+                //connection.createStatement().execute("DROP ALL OBJECTS");
                 connection.close();
             }
         } catch (SQLException se) {
             se.printStackTrace();
         }
     }
-    public ResultSet executeSQLStatement(String sql){
-        try (Statement statement = connection.createStatement()) {
-            ResultSet result = statement.executeQuery(sql);
-            return result;
-        } catch (SQLException e) {
-            e.getStackTrace();
-        }
-        return null;
+
+    public void passTest9() throws SQLException {
+        String sql = "ALTER TABLE COMPANY ALTER COLUMN ID RESTART WITH 1";
+        connection.createStatement().execute(sql);
+        sql = "ALTER TABLE CAR ALTER COLUMN ID RESTART WITH 1";
+        connection.createStatement().execute(sql);
+        sql = "ALTER TABLE CUSTOMER ALTER COLUMN ID RESTART WITH 1";
+        connection.createStatement().execute(sql);
     }
 
 
@@ -66,7 +65,8 @@ public class Database {
     }
 
     public void createCOMPANYTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS COMPANY (\n"
+        String sql =
+                 "CREATE TABLE IF NOT EXISTS COMPANY (\n"
                 + "     NAME VARCHAR(255) NOT NULL , \n"
                 + "     ID INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL  ,\n"
                 + "     UNIQUE(NAME)\n"
@@ -83,26 +83,195 @@ public class Database {
         Map<Integer,String> company_names = new HashMap<>();
         
         String sql = "SELECT * FROM COMPANY;";
+        return getIntegerStringMap(company_names, sql);
+    }
+
+    public void createCARTable(){
+        String sql = "CREATE TABLE IF NOT EXISTS CAR (\n"
+                + "     NAME VARCHAR(255) NOT NULL , \n"
+                + "     ID INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL  ,\n"
+                + "     COMPANY_ID INTEGER  NOT NULL  ,\n"
+                + "     foreign key (COMPANY_ID) references COMPANY(ID) ,\n"
+                + "     UNIQUE(NAME)\n"
+                + ");";
+
+        createTable(sql);
+    }
+
+    public void createCAREntry(String name, int ID) throws SQLException {
+        String sql = String.format("INSERT INTO CAR (NAME,COMPANY_ID) VALUES ('%s','%s');",name,ID);
+        connection.createStatement().execute(sql);
+
+    }
+    public Map<Integer,String> listCARValues(int ID) throws SQLException {
+        Map<Integer,String> car_names = new HashMap<>();
+
+        String sql = "SELECT * FROM CAR where COMPANY_ID = '"+ID+"'";
+        return getIntegerStringMap(car_names, sql);
+    }
+
+    public Map<Integer,String> listCompanyValues(int ID) throws SQLException {
+        Map<Integer,String> car_names = new HashMap<>();
+
+        String sql = "SELECT * FROM Company where ID = '"+ID+"'";
+        return getIntegerStringMap(car_names, sql);
+    }
+
+
+    public Map<Integer, String> getIntegerStringMap(Map<Integer, String> car_names, String sql) throws SQLException {
         ResultSet companies = connection.createStatement().executeQuery(sql);
         if (companies == null){
-            return company_names;
+            return car_names;
         }
 
         while (companies.next()){
             String name = companies.getString("NAME");
             int id = companies.getInt("ID");
-            
-            company_names.put(id,name);
+
+            car_names.put(id,name);
         }
-        return company_names;
+        return car_names;
     }
 
-    public void createCARTable(){
-        String sql = "CREATE TABLE IF NOT EXISTS COMPANY (\n"
+    public void createCUSTOMERTable(){
+        String sql = "CREATE TABLE IF NOT EXISTS CUSTOMER (\n"
                 + "     NAME VARCHAR(255) NOT NULL , \n"
                 + "     ID INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL  ,\n"
+                + "     RENTED_CAR_ID INTEGER  ,\n"
+                + "     foreign key (RENTED_CAR_ID) references CAR(ID) ,\n"
                 + "     UNIQUE(NAME)\n"
                 + ");";
 
+        createTable(sql);
     }
+
+    public static void returnCar(int customerAccountID) throws SQLException {
+        String sql = String.format("UPDATE CUSTOMER SET RENTED_CAR_ID = NULL WHERE ID = '%d'",customerAccountID);
+        connection.createStatement().executeUpdate(sql);
+    }
+
+    public static void rentCar(int carId, int customerAccountID) throws SQLException {
+        String sql = String.format("UPDATE CUSTOMER SET RENTED_CAR_ID = '%d' WHERE ID = '%d'",carId,customerAccountID);
+
+        connection.createStatement().executeUpdate(sql);
+    }
+
+
+
+    public void createCUSTOMEREntry(String name) throws SQLException {
+        String sql = String.format("INSERT INTO CUSTOMER (NAME,RENTED_CAR_ID) VALUES ('%s',NULL);",name);
+        connection.createStatement().execute(sql);
+
+    }
+    public Map<Integer,String> listCUSTOMERS() throws SQLException {
+        Map<Integer,String> customer_names = new HashMap<>();
+
+        String sql = "SELECT * FROM CUSTOMER";
+        return getIntegerStringMap(customer_names, sql);
+    }
+
+    //null for all cars
+    //user for username
+    public Map<Integer, Object[]> getAvailableCars(int owner,int flag) throws SQLException {
+        Map<Integer,Object[]> availableCars = new HashMap<>();
+        String sql;
+
+            sql = String.format("                    SELECT car.id, car.name, car.company_id \n" +
+                    "                    FROM car LEFT JOIN customer \n" +
+                    "                    ON car.id = customer.rented_car_id \n" +
+                    "                    WHERE customer.name IS NULL ;",owner);
+
+
+        return getIntegerMap(availableCars, sql);
+    }
+
+    public Map<Integer, Object[]> getIntegerMap(Map<Integer, Object[]> availableCars, String sql) throws SQLException {
+        ResultSet carsFound = connection.createStatement().executeQuery(sql);
+
+        if (carsFound == null){
+            return availableCars;
+        }
+
+        while (carsFound.next()){
+            String name = carsFound.getString("car.name");
+            int id = carsFound.getInt("car.id");
+            int company_id = carsFound.getInt("car.company_id");
+
+
+            Object[] list = new Object[2];
+            list[0] = company_id;
+            list[1] = name;
+            availableCars.put(id,list);
+
+        }
+        return availableCars;
+    }
+
+    public Map<Integer, Object[]> getAvailableCars(String owner,int flag) throws SQLException {
+        Map<Integer,Object[]> availableCars = new HashMap<>();
+        String sql;
+
+        if (flag == 0) {
+
+            sql = String.format(" SELECT car.ID, car.NAME, car.COMPANY_ID \n" +
+                    "                    FROM car LEFT JOIN customer \n" +
+                    "                    ON car.ID = customer.RENTED_CAR_ID \n" +
+                    "                    WHERE customer.NAME = '%s';", owner);
+        }
+        else{
+            sql = String.format("SELECT CAR.id as id, CAR.name as name, CAR.company_id as company_id FROM CAR LEFT JOIN CUSTOMER ON CAR.id = CUSTOMER.rented_car_id " +
+                    "WHERE company_id = '%s' AND CUSTOMER.rented_car_id is null;",owner);
+        }
+
+        return getIntegerMap(availableCars, sql);
+    }
+
+    public static Map getAllUserData() throws SQLException {
+        Map<Integer,Object> availableCars = new HashMap<>();
+
+        String sql = String.format("Select * from Customer");
+
+        ResultSet carsFound = connection.createStatement().executeQuery(sql);
+
+        if (carsFound == null){
+            return availableCars;
+        }
+
+        while (carsFound.next()){
+            String name = carsFound.getString("NAME");
+            int id = carsFound.getInt("ID");
+            int rented_car_id = carsFound.getInt("RENTED_CAR_ID");
+
+
+            Object[] list = new Object[2];
+            list[0] = rented_car_id;
+            list[1] = name;
+            availableCars.put(id,list);
+
+        }
+        return availableCars;
+    }
+
 }
+
+
+
+/**
+ * Customer
+ * get all customer data
+ * 1. Select * from Customer
+ *  return {customerID,name}
+ *
+ *  get customer rented car
+ * 2. Select Car.name, car.company_id, car.id
+ *      from customer union car
+ *      where customer.name == name and car.id == customer.rented_car_id
+ *
+ * find non rented cars
+ * 3. Select Car.name, car.company_id, car.id
+ *      from customer union car
+ *      where customer.rented_car_id == null
+ *
+ *4. select * from company where company.id == ID
+ *
+ */
--- Car Sharing/task/src/carsharing/Main.java
+++ Car Sharing/task/src/carsharing/Main.java
@@ -3,7 +3,7 @@ package carsharing;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -23,19 +23,77 @@ public class Main {
 
     public static int manager_options(Scanner scanner, Database database){
         System.out.println("1. Log in as a manager\n" +
+                "2. Log in as a customer\n" +
+                "3. Create a customer\n" +
                 "0. Exit");
         int input = scanner.nextInt();
         return input;
     }
 
-    public static void company_list(Database database) throws SQLException {
+    public static void car_list(Database database,int input) throws SQLException {
+        Map<Integer,String> companies = database.listCARValues(input);
+        int counter = 1;
+
+        if (companies.size() == 0 ){
+            System.out.println("The car list is empty!\n");
+        }else {
+            for (String value : companies.values()){
+                System.out.printf("%d. %s\n",counter++,value);
+
+            }
+        }
+    }
+
+    public static void create_car(int input,Database database,Scanner scanner) throws SQLException {
+        System.out.println("Enter the car name:\n");
+        scanner.nextLine();
+        String car_name = scanner.nextLine();
+
+        database.createCAREntry(car_name.strip(),input);
+
+        System.out.println("The car was added!\n");
+
+    }
+    public static void car_menu(Scanner scanner, Database database,Map<Integer,String> companies, int input) throws SQLException {
+        int exit = 1;
+        while (exit==1) {
+            System.out.printf("'%s' company:\n", companies.get(input));
+            System.out.println("1. Car list\n" +
+                    "2. Create a car\n" +
+                    "0. Back");
+            int car_choice = scanner.nextInt();
+            switch (car_choice) {
+                case 0:
+                    return;
+                case 1:
+                    car_list(database,input);
+                    break;
+                case 2:
+                    create_car(input,database,scanner);
+                    break;
+            }
+        }
+
+    }
+
+    public static void company_list(Database database, Scanner scanner) throws SQLException {
         Map<Integer,String> companies = database.listCOMPANYValues();
         if (companies.size() == 0 ){
             System.out.println("The company list is empty!\n");
         }else {
-            companies.forEach((key,value) -> System.out.printf("%d. %s",key,value));
+            System.out.println("Choose a company: \n");
+            companies.forEach((key,value) -> System.out.printf("%d. %s\n",key,value));
+            System.out.println("0. Back\n");
+            int input = scanner.nextInt();
+
+            if (input ==0){
+                return;
+            }
+            car_menu(scanner,database,companies,input);
+
         }
-        System.out.println("\n");
+
+
     }
     public static void create_company(Database database,Scanner scanner) throws SQLException {
         System.out.println("Enter the company name:");
@@ -62,7 +120,7 @@ public class Main {
                 case 0:
                     return;
                 case 1:
-                    company_list(database);
+                    company_list(database,scanner);
                     break;
                 case 2:
                     create_company(database,scanner);
@@ -72,6 +130,196 @@ public class Main {
             }
         }
     }
+    //rent car
+    // need a way to list all companies that have free cars
+    // need to check I do not have a car rented
+    // need to update customer, car and maybe company
+
+    //return car
+    // null out car rented
+
+    //current rented car
+    // uses same function as return car
+
+    public static void rent_car(int company_ID, String customer_name,Database database,Scanner scanner,int customer_id,Map<Integer, Object[]> cars ) throws SQLException {
+        System.out.println("Choose a car:\n");
+        Map<Integer, String> companies = database.listCOMPANYValues();
+        String[] car_names = new String[cars.size()];
+        String company_name = companies.get(company_ID);
+        int counter = 0;
+
+
+        for (int key:cars.keySet()){
+            Object[] entry = cars.get(key);
+            if (entry[0].equals(company_ID)){
+                car_names[counter] = (String) entry[1];
+                System.out.printf("%d. %s\n",++counter,entry[1]);
+
+            }
+
+        }
+        System.out.printf("0. Back\n");
+        int input = scanner.nextInt();
+
+        if (input == 0){
+            return;
+        }else{
+            database.rentCar(input,customer_id);
+            System.out.printf("You rented '%s'\n",car_names[input-1]);
+
+        }
+
+    }
+
+    public static int current_rented_car(Database database,String owner) throws SQLException {
+        Map<Integer, Object[]> current_car = database.getAvailableCars(owner,0);
+        Map<Integer,String> companies =  database.listCOMPANYValues();
+        Object[] car = new Object[2];
+
+        for (int key: current_car.keySet()){
+            car = current_car.get(key);
+        }
+
+        if (car[0]!=null){
+            System.out.printf("Your rented car:\n" +
+                    "%s\n" +
+                    "Company:\n" +
+                    "%s\n",car[1],companies.get(car[0]));
+            return 1;
+        }else{
+            System.out.println("You didn't rent a car!\n");
+            return 0;
+        }
+    }
+    public static void customer_rent_car(Database database,Scanner scanner,String name, int customer_id) throws SQLException {
+        Map<Integer, Object[]> cars = database.getAvailableCars(customer_id,0);
+        cars.forEach((key, value) -> System.out.printf("%d. %s\n", key, Arrays.toString(value)));
+        System.out.println("size "+cars.size());
+        ArrayList<Integer> dup_checker = new ArrayList<>();
+
+        int input = 1;
+        if (cars.size() == 0) {
+            System.out.println("The company list is empty!\n");
+            return;
+        } else {
+            if (database.getAvailableCars(name,0).size()>0) {
+                System.out.println("You've already rented a car!");
+                return;
+
+            } else {
+                while (input != 0) {
+                    for (int i = 1;i<cars.size()+1;i++) {
+                        if (dup_checker.contains(cars.get(i)[0])){
+
+                        }else{
+                            dup_checker.add((Integer) cars.get(i)[0]);
+                        }
+                    }
+
+
+                    System.out.println("Choose a company:");
+                    for (int i = 0;i<dup_checker.size();i++) {
+                        int company_ID = dup_checker.get(i);
+
+                        Map<Integer, String> companies = database.listCompanyValues(company_ID);
+                        String company_name = companies.get(company_ID);
+                        System.out.printf("%d. %s\n", i+1, company_name);
+                    }
+                    System.out.println("0. Back\n");
+
+                    input = scanner.nextInt();
+
+                    if (input==0){
+                        return;
+                    }else{
+                        rent_car(input,name,database,scanner,customer_id,cars);
+                        input = 0;
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+    public static void customer_rent_car_menu(Database database, Integer customer_number, Scanner scanner, String customer_name) throws SQLException {
+        int input = 1;
+
+        while (input != 0){
+            System.out.println("1. Rent a car\n" +
+                    "2. Return a rented car\n" +
+                    "3. My rented car\n" +
+                    "0. Back");
+            input = scanner.nextInt();
+            switch (input){
+                case 1:
+                    customer_rent_car(database,scanner,customer_name,customer_number);
+                    break;
+                case 2:
+                    customer_return_car(database,customer_number,customer_name);
+                    break;
+                case 3:
+                    current_rented_car(database,customer_name);
+                    break;
+                case 0:
+                    return;
+            }
+        }
+
+    }
+
+    private static void customer_return_car(Database database, int customer_id,String customer_name) throws SQLException {
+        if (current_rented_car(database,customer_name)==1) {
+            database.returnCar(customer_id);
+            System.out.println("You've returned a rented car!\n");
+        }else{
+            System.out.println("You didn't rent a car!\n");
+        }
+    }
+
+
+    public static Map<Integer,String> customer_listing(Database database) throws SQLException {
+        Map<Integer,String> companies = database.listCUSTOMERS();
+        return companies;
+
+    }
+    public static void customer_login(Scanner scanner, Database database) throws SQLException {
+        Map<Integer,String> customers = customer_listing(database);
+        int counter = 1;
+        int input = 1;
+
+        if (customers.size() == 0 ){
+            System.out.println("The customer list is empty!\n");
+            return;
+        }else {
+            while (input != 0) {
+                System.out.println("Customer list:");
+                for (String value : customers.values()) {
+                    System.out.printf("%d. %s\n", counter++, value);
+                }
+                System.out.println("0. Back\n");
+                input = scanner.nextInt();
+
+                customer_rent_car_menu(database,input,scanner,customers.get(input));
+                counter = 1;
+
+
+            }
+
+
+        }
+
+    }
+
+    public static void create_customer(Database database,Scanner scanner) throws SQLException {
+        System.out.println("Enter the customer name:\n");
+        scanner.nextLine();
+        String name = scanner.nextLine();
+        database.createCUSTOMEREntry(name);
+        System.out.println("The customer was added!\n");
+    }
 
     public static void main(String[] args) throws SQLException {
         String dbName = filename_parser(args);
@@ -80,19 +328,33 @@ public class Main {
 
         Database database = Database.createDB(dbName);
         database.createCOMPANYTable();
+        database.createCARTable();
+        database.createCUSTOMERTable();
+        database.passTest9();
 
 
         while (exit==0){
             int input = manager_options(scanner,database);
-            if (input == 0) {
-                database.closeDB();
-                return;
-            }else{
-                company_menu(scanner,database);
+            switch (input){
+                case 0:
 
+                    database.closeDB();
+                    return;
+                case 1:
+                    company_menu(scanner,database);
+                    break;
+                case 2:
+                    customer_login(scanner,database);
+                    break;
+                case 3:
+                    create_customer(database,scanner);
+                    break;
             }
         }
 
 
     }
 }
+
+
+//alter table car to return and rent
